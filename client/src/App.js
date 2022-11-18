8ef612bfcf48d339ab05211cf78ec807ee4d638b
Aladdin Mhemed <admhemed@gmail.com>
2022-11-18 11:31:03 +0300

    add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import PostCreate from './PostCreate';
 const app = () => {
-  return <div>Blog app</div>
+  return <div className={"container"}>
+    <h1>
+      Create Post
+    </h1>
+    <PostCreate/>
+  </div>
 };
 export default app;
 
